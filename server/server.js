c12643ce02b1340547717e04f97c3ce1c78f3d47
Muhammad Adnan <adnukunhome7@gmail.com>
2024-11-16 22:21:22 -1200

    Installed all dependencies.

---
+++ server/server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cookieparser = require('cookie-parser');
+const cors = require('cors');
+
+
+mongoose
+    .connect(
+        'mongodb+srv://adnukunhome7:Brazlow_567074@cluster0.hhcwc.mongodb.net/'
+    )
+    .then(() => console.log('MongoDB connected...'))
+    .catch((error) => console.log(error));
+
+const App = express();
+const PORT = process.env.PORT || 5000;
+
+App.use(
+    cors({
+        origin: 'http://localhost:5173/',
+        methods: ['GET', 'POST', 'DELETE', 'PUT'],
+        allowedHeaders: [
+            "Contect-Type",
+            "Authorization",
+            "Cache-Control",
+            "Expires",
+            "Pragma"
+        ],
+        credentials: true
+    })
+);
+
+App.use(cookieparser());
+App.use(express.json());
+
+App.listen(PORT, () => console.log('Server is running on the port ${PORT}'));
